2c44f7f5712f2520ee51b1b3c97c13280117ebf7
Pavel Krakhmalchyk <pavel.krakhmalchyk@leverx.com>
2019-01-11 02:39:14 +0300

    added adapt to task 1-2 lect pract and improved hw2

---
+++ Lection practice 1/Task 2/scripts/script.js
@@ -0,0 +1,26 @@
+function generateNewSender(){
+    var image = document.createElement("img");
+    image.src = "../Task 2/images/sender-photo.jpg";
+
+    var senderPhoto = document.createElement("div");
+    senderPhoto.classList.add("sender-photo");
+    senderPhoto.appendChild(image);
+
+    var senderName = document.createElement("div");
+    senderName.classList.add("sender-name");
+    senderName.textContent = "Marilyn Monroe";
+
+    var sender = document.createElement("div");
+    sender.classList.add("sender");
+    sender.appendChild(senderPhoto);
+    sender.appendChild(senderName);
+
+    return sender;
+}
+
+var sendersContainer = document.getElementsByClassName("senders-container")[0];
+
+for (i = 0; i < 10; i++){
+    var sender = generateNewSender();
+    sendersContainer.appendChild(sender);
+}
