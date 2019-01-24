196393c00dc409a938e3c291e0bb06446533bb3d
Pavel Krakhmalchyk <pavel.krakhmalchyk@leverx.com>
2019-01-24 12:39:54 +0300

    Made SAPUI5 part in HW8 & HW9

---
+++ Homework 8/Task 3/Component.js
@@ -0,0 +1,35 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel",
+	"./controller/HelloDialog"
+], function (UIComponent, JSONModel, HelloDialog) {
+	"use strict";
+
+	return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
+		metadata : {
+            manifest: "json"
+        },
+		init : function () {
+			UIComponent.prototype.init.apply(this, arguments);
+			var oData = {
+				recipient : {
+					name : "World"
+				}
+			};
+			var oModel = new JSONModel(oData);
+            this.setModel(oModel);
+			
+			this._helloDialog = new HelloDialog(this.getRootControl());
+			},
+
+			exit : function() {
+				this._helloDialog.destroy();
+				delete this._helloDialog;
+			},
+	
+			openHelloDialog : function () {
+				this._helloDialog.open();
+		    }
+	});
+
+});
